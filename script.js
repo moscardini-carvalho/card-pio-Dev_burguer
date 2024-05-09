135f00ba5c053e5b2ed6a4efc7e00088ed3f96a4
João Pedro <moscardinicarvalho1234@gmail.com>
2024-05-09 20:33:33 -0300

    Atualizações do código

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ const cartCounter = document.getElementById("cart-count")
 const addressInput = document.getElementById("address")
 const addressWarn = document.getElementById("address-warn")
 
+let cart = [];
+
 // ABRIR O MODAL DO CARRINHO
 cartBtn.addEventListener("click", function () {
     cartModal.style.display = "flex"
@@ -25,14 +27,67 @@ closeModalBtn.addEventListener("click", function () {
     cartModal.style.display = "none"
 })
 
-menu.addEventListener("click", function(event){
+menu.addEventListener("click", function (event) {
     let parentButton = event.target.closest(".add-to-cart-btn")
-    
-    if(parentButton){
+
+    if (parentButton) {
         const name = parentButton.getAttribute("data-name")
-        const price = parentButton.getAttribute("data-price")
-        
-        console.log(name)
-        console.log(price)
+        const price = parseFloat(parentButton.getAttribute("data-price"))
+
+        // ADICIONAR NO CARRINHO
+        addToCart(name, price)
+
     }
 })
+
+// FUNÇÃO PARA ADICIONAR NO CARRINHO
+
+function addToCart(name, price) {
+    const existingItem = cart.find(item => item.name === name)
+
+    if (existingItem) {
+        // SE O ITEM JÁ EXISTE, AUMENTA APENAS A QUANTIDADE +1
+        existingItem.quantity += 1;
+    } else {
+        cart.push({
+            name,
+            price,
+            quantity: 1,
+        })
+    }
+
+    updateCartModal()
+
+}
+
+// ATUALIZAÇÃO DO CARRINHO
+
+function updateCartModal() {
+    cartItemsContainer.innerHTML = "";
+    let total = 0;
+
+    cart.forEach(item => {
+        const cartItemElement = document.createElement("div");
+        cartItemElement.innerHTML = `
+            <div>
+                <div>
+                    <p>${item.name}</p>
+                    <p>${item.quantity}</p>
+                    <p>${item.price}</p>
+                </div>            
+
+                <div>
+                    <button>
+                        remover
+                    </button>
+                </div>
+            </div>
+          `
+
+        cartItemsContainer.appendChild(cartItemElement)
+
+    })
+
+
+
+}
